ecabf5bed9b0a721cb193c657ac9dd72896a9401
brahim boutal <37124910+Ibrahimboutal@users.noreply.github.com>
2025-05-27 11:33:00 +0800

    Delete project/dist directory

--- project/dist/assets/Delete-CbxsixXQ.js
+++
@@ -1,2 +0,0 @@
-import{ag as t,ah as a,j as r}from"./index-z-43rxTh.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,o=u(a()),i=r;l=e.default=(0,o.default)((0,i.jsx)("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}),"Delete");export{l as d};
-//# sourceMappingURL=Delete-CbxsixXQ.js.map
